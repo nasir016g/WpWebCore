97a2949290a1a05a00be3473ead482b9c374bcd1
Unknown <nasir016@gmail.com>
2020-05-25 23:05:17 +0200

    change home controller 2

--- WpCoreSolution/Presentation/Wp.Web.Api/Controllers/HomeController.cs
+++ WpCoreSolution/Presentation/Wp.Web.Api/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Wp.Web.Api.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page 2.";
 
             return View();
         }
