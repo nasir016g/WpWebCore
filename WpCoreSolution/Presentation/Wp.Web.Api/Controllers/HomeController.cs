99c67b726253f7c610ef7b4ba66c7f0d9066e77f
Nasir Mohammad <nasir016@gmail.com>
2020-05-29 00:12:57 +0200

    Modified home controller 6

--- WpCoreSolution/Presentation/Wp.Web.Api/Controllers/HomeController.cs
+++ WpCoreSolution/Presentation/Wp.Web.Api/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Wp.Web.Api.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page 5.";
+            ViewData["Message"] = "Your application description page 6.";
 
             return View();
         }
